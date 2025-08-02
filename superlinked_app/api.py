cec23eb845164f8ac4cc3ef2521548c4d525b8f3
Aneesh Naresh <aneesh.naresh@gmail.com>
2025-08-02 17:29:46 -0400

    Working on the data loader

--- superlinked_app/api.py
+++ superlinked_app/api.py
@@ -3,3 +3,10 @@ from loguru import logger
 
 from superlinked_app import index, query
 from superlinked_app.config import settings
+
+product_source: sl.RestSource = sl.RestSource(index.product)
+
+product_data_loader_parse = sl.DataFrameParser(
+    index.product, mapping={index.product.id: "asin"}
+)
+product_data_loader_config = sl.DataLoaderConfig(str(settings))
